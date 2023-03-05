5305017687887a64204ab9e1f26fe271c9094fa4
Proteam01 <alberto.alpuche.79@hotmail.com>
2023-03-05 16:49:49 -0600

    html parameters set

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',hello="test2")
 
 if __name__ == "__main__":
-    app.run(host='localhost',port=5000,debug=True)
+    app.run(host='localhost',port=8080,debug=True)
